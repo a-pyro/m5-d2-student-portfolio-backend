5f1b29336e8f613fbc7e47f603c157daecdcf25a
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-04-14 14:58:05 +0200

    adds basica get route

--- src/projects/index.js
+++ src/projects/index.js
@@ -5,4 +5,20 @@ import fs from 'fs';
 
 const router = express.Router();
 
+/* leggo e collego il mio file json */
+const __path = fileURLToPath(import.meta.url);
+const __dirname = dirname(__path);
+const projectsJSONpath = join(__dirname, 'projects.json');
+// console.log(projectsJSONpath);
+const getProjects = () =>
+  JSON.parse(fs.readFileSync(projectsJSONpath.toString()));
+
+const writeProjects = (projectsArr) =>
+  fs.writeFileSync(projectsJSONpath, JSON.stringify(projectsArr));
+
+router.get('/', (req, res, next) => {
+  const projects = getProjects();
+  res.send(projects);
+});
+
 export default router;
