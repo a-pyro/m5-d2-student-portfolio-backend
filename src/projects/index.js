2d24d6298ccb82ddb51418e4b5ccc601c7014709
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-04-14 14:30:16 +0200

    a

---
+++ src/projects/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+
+const router = express.Router();
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import studentsRouter from './students/index.js';
+import projectsRouter from './projects/index.js';
 import listEndpoints from 'express-list-endpoints';
 import cors from 'cors';
 
 const server = express();
-const port = 3001;
+const port = process.env.PORT || 3002;
 
 server.use(cors());
 server.use(express.json()); // così gli dico che i body sono json, altrimenti arrivanpo undefinded
+//va prima dell'use sulle route
 server.use('/students', studentsRouter); //usa questa parte comune di path per tutti gli endpoint
+server.use('/projects', projectsRouter);
+
+/* Qui ci vanno gli error middlewares */
 
 console.log(listEndpoints(server));
 server.listen(port, () => {
